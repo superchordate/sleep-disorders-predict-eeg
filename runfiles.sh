dcc41cb34b7cd6f5543bd24fe343774c051749ed
Superchordate <superchordate@gmail.com>
2020-05-10 20:59:18 -0500

    Fix location for runfiles.sh

--- runfiles.sh
+++ runfiles.sh
@@ -1,14 +1,14 @@
-cd /scratch/midway2/bchamberlain/bigdata-2020-project/
+cd /project2/msca/bchamberlain/bigdata-2020-project/
 git pull
 rm -R log
 mkdir log
 sbatch tocsv.sbatch -N1 -n1
 
 # aliases we can use.
-alias err='cat /scratch/midway2/bchamberlain/bigdata-2020-project/log/edf2csv.err'  
-alias out='cat /scratch/midway2/bchamberlain/bigdata-2020-project/log/edf2csv.out'
-alias csv='ls -ls /scratch/midway2/bchamberlain/bigdata-2020-project/csv'  
-alias done='ls /scratch/midway2/bchamberlain/bigdata-2020-project/csv | grep "done"'
+alias err='cat /project2/msca/bchamberlain/bigdata-2020-project/log/edf2csv.err'  
+alias out='cat /project2/msca/bchamberlain/bigdata-2020-project/edf2csv.out'
+alias csv='ls -ls /project2/msca/bchamberlain/bigdata-2020-project/csv'  
+alias done='ls /project2/msca/bchamberlain/bigdata-2020-project/csv | grep "done"'
 
 watch squeue -u bchamberlain
 
