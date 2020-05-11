28cc8f40c2867c02de686533139edf6587de1ee3
Superchordate <superchordate@gmail.com>
2020-05-10 19:55:26 -0500

    Less memory

--- runfiles.sh
+++ runfiles.sh
@@ -3,15 +3,19 @@ git pull
 rm -R log
 mkdir log
 sbatch tocsv.sbatch
+
+# aliases we can use.
+alias err='cat /scratch/midway2/bchamberlain/bigdata-2020-project/log/edf2csv.err'  
+alias out='cat /scratch/midway2/bchamberlain/bigdata-2020-project/log/edf2csv.out'
+alias csv='ls -ls /scratch/midway2/bchamberlain/bigdata-2020-project/csv'  
+alias done='ls /scratch/midway2/bchamberlain/bigdata-2020-project/csv | grep "done"'
+
 watch squeue -u bchamberlain
-
-cat /scratch/midway2/bchamberlain/bigdata-2020-project/log/edf2csv.err 
-cat /scratch/midway2/bchamberlain/bigdata-2020-project/log/edf2csv.out
-
-ls /scratch/midway2/bchamberlain/bigdata-2020-project/csv
-
-watch ls /scratch/midway2/bchamberlain/bigdata-2020-project/csv | grep 'done'
 
 # clear output:
 # rm -R /scratch/midway2/bchamberlain/bigdata-2020-project/csv
 
+cd /scratch/midway2/bchamberlain/bigdata-2020-project/
+git pull
+sbatch fileinfo.sbatch
+watch squeue -u bchamberlain
