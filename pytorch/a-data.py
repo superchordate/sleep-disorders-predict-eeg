bdbd7a2dc8fd8c4a0f5012e6e084534301fe4df8
Superchordate <superchordate@gmail.com>
2020-05-31 12:13:17 -0500

    Fix model
    And use GPU

--- pytorch/a-data.py
+++ pytorch/a-data.py
@@ -3,6 +3,7 @@ import os, re
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+import multiprocessing
 
 class EEGDataset(Dataset):
 
@@ -20,24 +21,25 @@ class EEGDataset(Dataset):
             idx = idx.tolist()
             
         x = pd.read_parquet(self.files[idx])
+        x = x.iloc[range(50000, x.shape[0]-50000), ]
         x = StandardScaler().fit_transform(x)
-        #x = x.iloc[100000:200000]
         
         y = (1 if re.match('.+' + self.disorder, self.files[idx]) else 0)
         
-        #x = torch.tensor(np.transpose(np.array(x))).type(torch.DoubleTensor)
-        #y = torch.tensor(np.array(y)).type(torch.DoubleTensor)
         x = torch.tensor(np.transpose(np.array(x))).float()
-        y = torch.tensor(np.array(y)).long()
+        y = torch.tensor(np.array(y)).long()  
 
         return x, y
 
-dataset = EEGDataset('train', 'plm')
+# I get errors whenever I set num_workers != 0
+usecores = multiprocessing.cpu_count() - 1
+
+dataset = EEGDataset('train', 'nfle')
 dataloader = DataLoader(
     # I've moved all the 128 files into the train/ folder, but you can use a different folder if you like.
     # the folder should contain all the parquet files.
-    dataset, batch_size = len(dataset), shuffle = True, num_workers = 0
+    dataset, 
+    #batch_size = int(round(len(dataset)/10,0)), 
+    batch_size = 10, 
+    shuffle = True, num_workers = 0
 )
-
-#for local_batch, local_labels in dataloader:
-#    t = local_batch.shape
--- pytorch/b-model.py
+++ pytorch/b-model.py
@@ -3,45 +3,47 @@ from torch.autograd import Variable
 from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, MaxPool1d, Module, Softmax, BatchNorm1d, Dropout, Conv1d
 from torch.optim import Adam, SGD
 
-
 # https://www.analyticsvidhya.com/blog/2019/10/building-image-classification-models-cnn-pytorch/
 # see more complicated eeg at https://github.com/aliasvishnu/EEGNet/blob/master/EEGNet-PyTorch.ipynb
 # more at https://github.com/topics/eeg-classification
-class Net(Module):   
+class Net(Module):
     def __init__(self):
         super(Net, self).__init__()
 
         self.cnn_layers = Sequential(
             # convolutional reduce 9 channels to 4
-            Conv1d(in_channels = 9, out_channels = 18, kernel_size = 100, stride = 50, padding = 0),
+            Conv1d(in_channels = 9, out_channels = 18, kernel_size = 1000, stride = 350, padding = 0),
             #BatchNorm1d(num_features = 18),
             ReLU(inplace = True),
             MaxPool1d(kernel_size = 100, stride = 50),
             # convolutional reduce 9 channels to 4
-            Conv1d(in_channels = 18, out_channels = 2, kernel_size = 100, stride = 50, padding = 0),
+            Conv1d(in_channels = 18, out_channels = 2, kernel_size = 10, stride = 5, padding = 0),
             #BatchNorm1d(num_features = 2),
             ReLU(inplace = True),
             MaxPool1d(kernel_size = 10, stride = 5),
-            Linear(in_features = 3, out_features = 1),
+            Linear(in_features = 5, out_features = 1),
+            # sigmoid will create output 0-1
+            Sigmoid(),
         )
         
     # Defining the forward pass
     def forward(self, x):
         x = self.cnn_layers(x)
-        x = x.view(x.size(0), -1)
         return x
     
 net = Net()
 criterion = CrossEntropyLoss()
 optimizer = Adam(net.parameters())
+if torch.cuda.is_available():
+    net = net.cuda()
+    criterion = criterion.cuda()
     
 
 # CUDA for PyTorch
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 torch.backends.cudnn.benchmark = True
 max_epochs = 100
-        
-# Loop over epochs
+
 for epoch in range(max_epochs):
     
     i = 0
@@ -50,10 +52,11 @@ for epoch in range(max_epochs):
     for local_batch, local_labels in dataloader:
 
         i += 1
+        local_labels = local_labels.view(local_labels.size(0), 1)
         
-        # Transfer to GPU
-        # breaks
-        #local_batch, local_labels = local_batch.to(device), local_labels.to(device)
+        if torch.cuda.is_available():
+            local_batch = local_batch.cuda()
+            local_labels = local_labels.cuda()
 
         # Model computations
         optimizer.zero_grad()
@@ -63,8 +66,20 @@ for epoch in range(max_epochs):
         optimizer.step()
         
         # print statistics
-        print('epoch %s; %s of %s; loss: %s' % (epoch, i, len(dataloader), loss.item()))
-        #running_loss += loss.item()
-        #if i % 2000 == 1999:    # print every 2000 mini-batches
-        #print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
-        #running_loss = 0.0
+        # prediction is the index of hte maximum.
+        predictions = np.array([int(i) for i in outputs.max(dim = 1)[1]])
+        labels =  np.array([int(i) for i in local_labels])
+        print(
+            'epoch %s; batch %s of %s; true : %s; loss: %s; accuracy: %s' % (
+            epoch, i, 
+            len(dataloader), 
+            sum(labels), 
+            round(loss.item(), 3),
+            np.mean(labels == predictions)
+        ))
+        
+        #print(labels)
+        #print(outputs)
+        #print(predictions)
+        
+        del local_batch, local_labels, predictions, labels
